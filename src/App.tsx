d8e82cfd65ce47294f80dfdfad3a9873af4fb227
Alvian Naya Pradhana <alvinnaya13@gmail.com>
2024-10-16 18:25:48 +0700

    add some ui

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
         <div className='w-full h-full flex items-center justify-center flex-col' >
             <h1 className='text-4xl text-orange-300 font-extrabold'>Starter : {state}</h1>
-            <button onClick={()=>{setState(state + 1)}}> add </button>
+            <button className='text-xl bg-orange-200 m-4 px-4 py-1 rounded-lg font-semibold' onClick={()=>{setState(state + 1)}}> add </button>
         </div>
        
     </>
